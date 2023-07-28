22ed347f86804680c3e702236547a554e9f71c68
Pedro Silva <progerios@hotmail.com>
2023-07-28 14:54:50 -0300

    Consulta simples com query methods

--- src/main/java/com/pedrosilva/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/pedrosilva/workshopmongo/repository/PostRepository.java
@@ -1,6 +1,10 @@
 package com.pedrosilva.workshopmongo.repository;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.pedrosilva.workshopmongo.domain.Post;
@@ -8,4 +12,8 @@ import com.pedrosilva.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
